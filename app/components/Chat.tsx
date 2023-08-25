a5d2ce1dad57f3c1de95c98a6b4486bfe50735f7
tatsu <tatsu72x@gmail.com>
2023-08-25 14:49:41 +0900

    ✨ framer-motionによるチャット表示のアニメーションを追加

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -1,7 +1,30 @@
 import { Message } from "../types/custom"
+import { motion } from "framer-motion"
+import { Flex } from "@chakra-ui/react"
 
 const Chat = ({ role, content }: Message) => {
-    return <></>
+    return (
+        <motion.div
+            style={{
+                alignSelf: role === "assistant" ? "flex-start" : "flex-end",
+                width: "auto",
+            }}
+            initial={{
+                opacity: 0,
+                translateY: "100%",
+            }}
+            animate={{ opacity: 1, translateY: 0, transition: { duration: 0.3 }}}
+            exit={{ opacity: 0, translateY: 0}}
+        >
+            <Flex
+                gap="5px"
+                w="full"
+                flexDir={role === "assistant" ? "row" : "row-reverse"}
+            >
+            </Flex>
+        </motion.div>
+    )
+}
 }
 
 export default Chat
