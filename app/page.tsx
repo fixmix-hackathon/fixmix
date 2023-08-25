890f0b971767a02c70cb68e53a9785f63d6eca63
tatsu <tatsu72x@gmail.com>
2023-08-25 14:45:58 +0900

    ✨ ChatGPT-APIと通信するためのロジックを追加
    動くかは知らない

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,57 @@
 "use client"
 
-import { NextPage } from "next";
+import { Flex } from "@chakra-ui/react"
+import type { NextPage } from "next"
+import { useState } from "react"
+import Chat from "./components/Chat"
 import InputForm from "./components/InputForm"
+import { Message } from "./types/custom"
 import ThreeDotsLoader from "./components/ThreeDotsLoader"
 
 const Home: NextPage = () => {
+  const [chats, setchats] = useState<Message[]>([
+    {
+      role: "system",
+      content: "#", // Python側から取得？
+    }
+  ])
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const handleSubmit = async (message: Message) => {
+    try {
+      setIsSubmitting(true);
+      setChats((prev) => [...prev, message]);
+
+      // ChatGPT APIと通信
+      const response = await fetch("/api/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: [...chats, message].map((d) => ({
+            role: d.role,
+            content: d.content,
+          })),
+        }),
+      })
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        )
+      }
+      setChats((prev) => [...prev, data.result as Message]);
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
   return (
     <div className="w-full max-w-2xl bg-white p-4 my-10">
     
