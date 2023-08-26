5708f9cb322ba1db92040cefa0cef3b3a1e992d2
tatsu <tatsu72x@gmail.com>
2023-08-26 19:16:35 +0900

    ✨ スクロールに応じてヘッダーを表示しなくする機能を実装

--- app/header.tsx
+++ app/header.tsx
@@ -1,9 +1,36 @@
+"use client";
+
+import React, { useState, useEffect } from "react"
 import { siteTitle } from "./constants/constants"
 import Link from "next/link"
 
 const Header = () => {
+    const [showHeader, setShowHeader] = useState(true)
+
+    useEffect(() => {
+        const handleScroll = () => {
+
+        const currentScrollPos = window.scrollY
+
+        if (currentScrollPos > 1200) {
+            setShowHeader(false)
+        } else {
+            setShowHeader(true)
+        }
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => {
+        window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
+
     return (
         <header
+        className={`relative py-5 bg-white shadow-md ${
+            showHeader ? "sticky top-0 z-10" : "hidden"
+        }`}
         >
         <div className="flex justify-between px-8">
             <Link
