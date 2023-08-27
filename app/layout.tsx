ee013b7642a6da5e49279c87c3d7f2d1514ba77d
tatsu <tatsu72x@gmail.com>
2023-08-27 22:55:41 +0900

    💄 ファビコン設定用のコードを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import './prism.css'
+// import './prism.css'
 import type { Metadata } from 'next'
 import Footer from './footer'
 import Header from './header'
@@ -23,6 +23,13 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
       <head />
       <body className="min-h-screen bg-white md:bg-gray-100">
         <Header />
