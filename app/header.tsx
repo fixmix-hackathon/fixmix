6a8828d8346025a0c4f02195cfcc8ec5a466afe1
tatsu <tatsu72x@gmail.com>
2023-08-28 06:22:55 +0900

    💄 ヘッダーの文字の大きさを更新

--- app/header.tsx
+++ app/header.tsx
@@ -35,13 +35,13 @@ const Header = () => {
         <div className="flex justify-between px-8">
             <Link
             href="/"
-            className="text-sm md:text-1xl text-gray-600 font-semibold whitespace-pre-line"
+            className="text-md md:text-1xl text-gray-600 font-semibold whitespace-pre-line"
             >
             {siteTitle}
             </Link>
             <Link
             href="/login"
-            className="text-sm md:text-1xl text-gray-600 font-semibold whitespace-pre-line"
+            className="text-md md:text-1xl text-gray-600 font-semibold whitespace-pre-line"
             >
             ログイン
             </Link>
