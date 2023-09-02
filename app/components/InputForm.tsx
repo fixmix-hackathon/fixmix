c95b93a80c3c27dc17af6303dddffbb0c2705769
Tatsuru Hisanaga <tatsu72x@gmail.com>
2023-09-02 15:41:25 +0900

    [change] InputFormをLexicalのリッチテキストエディタに変更
    現状では、入力したメッセージの送信（チャット）はできません。

--- app/components/InputForm.tsx
+++ app/components/InputForm.tsx
@@ -1,5 +1,8 @@
 import React, { useRef } from "react";
 import { Message } from "../types/custom";
+import Editor from "./editor/Editor";
+import "../../styles.css";
+
 
 type InputFormProps = {
     onSubmit: (message: Message) => Promise<void>
@@ -27,7 +30,7 @@ const InputForm = ({ onSubmit }: InputFormProps) => {
         onSubmit={handleSubmit}
         className="flex items-center p-4 border-t border-gray-200"
         >
-            <textarea
+            {/* <textarea
                 ref={textareaRef}
                 className="flex-grow text-gray-600 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:border-blue-300"
                 placeholder="メッセージを入力..."
@@ -38,14 +41,15 @@ const InputForm = ({ onSubmit }: InputFormProps) => {
                         console.log('Enter key pressed!!!');
                     }
                 }}
-            />
-        <button
+            /> */}
+            <Editor />
+        {/* <button
             type="submit"
             className="group relative px-4 py-2 overflow-hidden rounded-lg bg-white text-lg shadow font-mono ml-4 px-4 py-2 text-black "
         >
             <div className="absolute inset-0 w-1.5 bg-amber-300 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
             <span className="relative text-black-600 font-semibold md:text-1xl group-hover:text-white">送信</span>
-        </button>
+        </button> */}
         </form>
     )
 }
