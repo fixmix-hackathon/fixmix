b816684e915175a06b51c0d21c4b7f5e5bb468f6
Tatsuru Hisanaga <tatsu72x@gmail.com>
2023-09-02 15:30:18 +0900

    [add] Lexicalのエディタにプラグインを導入

--- app/components/editor/Editor.tsx
+++ app/components/editor/Editor.tsx
@@ -6,7 +6,7 @@ import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 // import TreeViewPlugin from "./plugins/TreeViewPlugin";
-// import ToolbarPlugin from "./plugins/ToolbarPlugin";
+import ToolbarPlugin from "./plugins/ToolbarPlugin";
 import { HeadingNode, QuoteNode } from "@lexical/rich-text";
 import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
 import { ListItemNode, ListNode } from "@lexical/list";
@@ -17,12 +17,12 @@ import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
 import { TRANSFORMERS } from "@lexical/markdown";
 
-// import ListMaxIndentLevelPlugin from "./plugins/ListMaxIndentLevelPlugin";
-// import CodeHighlightPlugin from "./plugins/CodeHighlightPlugin";
-// import AutoLinkPlugin from "./plugins/AutoLinkPlugin";
+import ListMaxIndentLevelPlugin from "./plugins/ListMaxIndentLevelPlugin";
+import CodeHighlightPlugin from "./plugins/CodeHighlightPlugin";
+import AutoLinkPlugin from "./plugins/AutoLinkPlugin";
 
 function Placeholder() {
-    return <div className="editor-placeholder">Enter some rich text...</div>;
+    return <div className="editor-placeholder">メッセージを入力...</div>;
 }
 
     const editorConfig = {
@@ -53,7 +53,7 @@ function Placeholder() {
     return (
         <LexicalComposer initialConfig={editorConfig}>
         <div className="editor-container">
-            {/* <ToolbarPlugin /> */}
+            <ToolbarPlugin />
             <div className="editor-inner">
             <RichTextPlugin
                 contentEditable={<ContentEditable className="editor-input" />}
@@ -63,11 +63,11 @@ function Placeholder() {
             <HistoryPlugin />
             {/* <TreeViewPlugin /> */}
             <AutoFocusPlugin />
-            {/* <CodeHighlightPlugin /> */}
+            <CodeHighlightPlugin />
             <ListPlugin />
             <LinkPlugin />
-            {/* <AutoLinkPlugin />
-            <ListMaxIndentLevelPlugin maxDepth={7} /> */}
+            <AutoLinkPlugin />
+            <ListMaxIndentLevelPlugin maxDepth={7} />
             <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
             </div>
         </div>
